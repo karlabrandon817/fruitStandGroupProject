84fb6e1b45679a62238761de49f2eeb228237483
Karla Hugo <karlabrandon817@gmail.com>
2016-12-01 16:18:59 -0600

    append marketprice to DOM

--- script.js
+++ script.js
@@ -4,33 +4,46 @@ $(document).ready(function(){
     var apples = {
       marketPrice: 1.00,
       avgPrice: '',
+      quantity: '',
       };// end apples
     var oranges= {
       marketPrice: 0.75,
       avgPrice: '',
+      quantity: '',
     };//end oranges
     var bananas= {
       marketPrice: 0.50,
       avgPrice: '',
+      quantity: '',
     };//end bananas
     var grapes= {
       marketPrice: 2.00,
       avgPrice: '',
+      quantity: '',
     };// end grapes
 
 
     //add objects to fruitArray
     fruitArray.push(apples, oranges, bananas, grapes);
 
-console.log(fruitArray);
-changePrice();
+    console.log(fruitArray);
 
+
+    //show MarketPrice on DOM
+    var showMarketPrice = function(){
+      //loop thru fruitArray and show each price
+      $('#applesMP').html(fruitArray[0].marketPrice);
+      $('#orangesMP').html(fruitArray[1].marketPrice);
+      $('#bananasMP').html(fruitArray[2].marketPrice);
+      $('#grapesMP').html(fruitArray[3].marketPrice);
+    };//end showMarketPrice
+    
+    //random price change function
+    var changePrice = function (){
+      for (var i = 0; i < fruitArray.length; i++) {
+        fruitArray[i].marketPrice += (Math.random()-0.5);
+      }
+      showMarketPrice();
+    };// end changePrice
+  changePrice();
 });//end of document ready function
-
-//random price change function
-var changePrice = function (){
-  for (var i = 0; i < fruitArray.length; i++) {
-    fruitArray[i].marketPrice += (Math.random()-0.5);
-  }
-
-};// end changePrice
