c565aaa8cbb55da47d18c7f119033fb7e77cf1b1
Karla Hugo <karlabrandon817@gmail.com>
2016-12-02 10:54:43 -0600

    average price function and AP to DOM

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 var fruitArray = [];//end fruitArray
+var appleArray = [];//end appleArray
+var orangeArray = [];//end orangeArray
+var bananaArray = [];//end bananaArray
+var grapeArray = [];//end grapeArray
 
 $(document).ready(function(){
     var apples = {
@@ -48,6 +52,7 @@ $(document).ready(function(){
 
 var userCash = 100;
 //display userCash to DOM
+$("#myCash").html(userCash);
 
     $('#applesB').on('click', function(){
       console.log(fruitArray);
@@ -55,25 +60,62 @@ var userCash = 100;
       $('#applesQ').html(fruitArray[0].quantity);
       console.log(userCash -= fruitArray[0].marketPrice);
       $("#myCash").html(userCash);
+      appleArray.push(fruitArray[0].marketPrice);
+      var count = 0;
+      for (var i = 0; i <appleArray.length; i++){
+        count = count + appleArray[i];
+      }
+      console.log(count);
+      $('#applesAP').html(count/appleArray.length);
     });
+
     $('#orangesB').on('click', function(){
       fruitArray[1].quantity += 1;
       $('#orangesQ').html(fruitArray[1].quantity);
       console.log(userCash -= fruitArray[1].marketPrice);
       $("#myCash").html(userCash);
+      orangeArray.push(fruitArray[1].marketPrice);
+      var count = 0;
+      for (var i = 0; i <orangeArray.length; i++){
+        count = count + orangeArray[i];
+      }
+      console.log(count);
+      $('#orangesAP').html(count/orangeArray.length);
     });
+
     $('#bananasB').on('click', function(){
       fruitArray[2].quantity += 1;
       $('#bananasQ').html(fruitArray[2].quantity);
       console.log(userCash -= fruitArray[2].marketPrice);
       $("#myCash").html(userCash);
+      bananaArray.push(fruitArray[2].marketPrice);
+      var count = 0;
+      for (var i = 0; i <bananaArray.length; i++){
+        count = count + bananaArray[i];
+      }
+      console.log(count);
+      $('#bananasAP').html(count/bananaArray.length);
     });
+
     $('#grapesB').on('click', function(){
       fruitArray[3].quantity += 1;
       $('#grapesQ').html(fruitArray[3].quantity);
       console.log(userCash -= fruitArray[3].marketPrice);
       $("#myCash").html(userCash);
+      grapeArray.push(fruitArray[3].marketPrice);
+      var count = 0;
+      for (var i = 0; i <grapeArray.length; i++){
+        count = count + grapeArray[i];
+      }
+      console.log(count);
+      $('#grapesAP').html(count/grapeArray.length);
     });
+
+
+    //alert userCash 0
+    if(userCash <= 0){
+      alert('You are broke');
+    }//end alert
 
 //calculate userCash
 
