2486418b0aa8758a7d028b64cf46f72670615324
Karla Hugo <karlabrandon817@gmail.com>
2016-12-02 09:19:16 -0600

    added quantity to DOM

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ var fruitArray = [];//end fruitArray
 $(document).ready(function(){
     var apples = {
       marketPrice: 1.00,
-      avgPrice: '',
-      quantity: '',
+      avgPrice: 0,
+      quantity: 0,
       };// end apples
     var oranges= {
       marketPrice: 0.75,
-      avgPrice: '',
-      quantity: '',
+      avgPrice: 0,
+      quantity: 0,
     };//end oranges
     var bananas= {
       marketPrice: 0.50,
-      avgPrice: '',
-      quantity: '',
+      avgPrice: 0,
+      quantity: 0,
     };//end bananas
     var grapes= {
       marketPrice: 2.00,
-      avgPrice: '',
-      quantity: '',
+      avgPrice: 0,
+      quantity: 0,
     };// end grapes
 
 
@@ -40,14 +40,33 @@ $(document).ready(function(){
     //random price change function
     var changePrice = function (){
       for (var i = 0; i < fruitArray.length; i++) {
-        fruitArray[i].marketPrice = (Number(fruitArray[i].marketPrice) + (Math.random()-0.5)).toLocaleString('en-US', { style:'currency', currency: 'USD'});
-      setInterval(changePrice, 15000);
+      console.log( fruitArray[i].marketPrice = (Number(fruitArray[i].marketPrice) + (Math.random()-0.5)).toLocaleString('en-US', { style:'currency', currency: 'USD'}));
+      //setInterval(changePrice, 15000);
       }
       showMarketPrice();
     };// end changePrice
-
-
     changePrice();
 
+    $('#applesB').on('click', function(){
+      console.log(fruitArray);
+      fruitArray[0].quantity += 1;
+      $('#applesQ').html(fruitArray[0].quantity);
+    });
+    $('#orangesB').on('click', function(){
+      console.log('working');
+      fruitArray[1].quantity += 1;
+      $('#orangesQ').html(fruitArray[1].quantity);
+    });
+    $('#bananasB').on('click', function(){
+      console.log('working');
+      fruitArray[2].quantity += 1;
+      $('#bananasQ').html(fruitArray[2].quantity);
+    });
+    $('#grapesB').on('click', function(){
+      console.log('working');
+      fruitArray[3].quantity += 1;
+      $('#grapesQ').html(fruitArray[3].quantity);
+    });
 
-});//end of document ready function
+
+});//end of document ready function 
