26fec124066ff18ff40b04538d8c8727fc0d116a
Karla Hugo <karlabrandon817@gmail.com>
2016-12-02 09:55:21 -0600

    diplay quantity of fruit to DOM

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ $(document).ready(function(){
       quantity: 0,
     };// end grapes
 
-
     //add objects to fruitArray
     fruitArray.push(apples, oranges, bananas, grapes);
 
@@ -40,33 +39,40 @@ $(document).ready(function(){
     //random price change function
     var changePrice = function (){
       for (var i = 0; i < fruitArray.length; i++) {
-      console.log( fruitArray[i].marketPrice = (Number(fruitArray[i].marketPrice) + (Math.random()-0.5)).toLocaleString('en-US', { style:'currency', currency: 'USD'}));
-      //setInterval(changePrice, 15000);
-      }
+      console.log( fruitArray[i].marketPrice = (Number(fruitArray[i].marketPrice))); //+ (Math.random()-0.5)).toLocaleString('en-US', { style:'currency', currency: 'USD'}));
+      // setInterval(changePrice, 15000);
+    }
       showMarketPrice();
     };// end changePrice
     changePrice();
+
+var userCash = 100;
 
     $('#applesB').on('click', function(){
       console.log(fruitArray);
       fruitArray[0].quantity += 1;
       $('#applesQ').html(fruitArray[0].quantity);
+      console.log(userCash -= fruitArray[0].marketPrice);
     });
     $('#orangesB').on('click', function(){
-      console.log('working');
       fruitArray[1].quantity += 1;
       $('#orangesQ').html(fruitArray[1].quantity);
+      console.log(userCash -= fruitArray[1].marketPrice);
     });
     $('#bananasB').on('click', function(){
-      console.log('working');
       fruitArray[2].quantity += 1;
       $('#bananasQ').html(fruitArray[2].quantity);
+      console.log(userCash -= fruitArray[2].marketPrice);
     });
-    $('#grapesB').on('click', function(){
-      console.log('working');
+    $('#grapesB').on('click', function(){ 
       fruitArray[3].quantity += 1;
       $('#grapesQ').html(fruitArray[3].quantity);
+      console.log(userCash -= fruitArray[3].marketPrice);
     });
 
+//calculate userCash
 
-});//end of document ready function 
+
+
+
+});//end of document ready function
